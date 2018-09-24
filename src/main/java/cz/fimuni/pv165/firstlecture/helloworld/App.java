66bc984bff7d0501a1669250eeb68ead39246dc3
x451570 <x451570@mail.muni.cz>
2018-09-24 09:31:14 +0200

    Initial commit

---
+++ src/main/java/cz/fimuni/pv165/firstlecture/helloworld/App.java
@@ -0,0 +1,13 @@
+package cz.fimuni.pv165.firstlecture.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
